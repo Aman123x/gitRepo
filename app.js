e9a88eff2637d18acfb86fab71b1bacea9eec4f7
Aman <aman01428@gmail.com>
2023-11-03 21:16:43 +0530

    03Nov

--- app.js
+++ app.js
@@ -41,22 +41,48 @@ header.style.borderBottom="solid 3px black";
 
 // Get Elements By TagName
 
-var li=document.getElementsByTagName("li");
-console.log(li);
-console.log(li[1]);
-li[1].textContent="hello 2";
-li[1].style.fontWeight="bold";
-li[1].style.backgroundColor="yellow";
+// var li=document.getElementsByTagName("li");
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent="hello 2";
+// li[1].style.fontWeight="bold";
+// li[1].style.backgroundColor="yellow";
 
-for(let i=0;i<li.length;i++){
-    if(i===1){
-        continue;
+// for(let i=0;i<li.length;i++){
+//     if(i===1){
+//         continue;
+//     }
+//     else if(i===2){
+//         li[i].style.backgroundColor="green";
+//     }
+//     else{
+//         li[i].style.backgroundColor="grey";
+//     }
+//     li[i].style.fontWeight="bold";
+// }
+
+// querySelector --> It always select first item
+
+let list=document.querySelector(".list-group-item");
+
+// list.style.backgroundColor="red";
+
+let secondList=document.querySelector(".list-group-item:nth-child(2)");
+
+// secondList.style.backgroundColor="green";
+
+let thirdList=document.querySelector(".list-group-item:nth-child(3)");
+
+// thirdList.style.display="none";
+
+//querySelectorAll --> Select all the items
+
+let listSecond=document.querySelectorAll(".list-group-item");
+
+listSecond[1].style.color="green";
+
+listSecond.forEach((listSecond,index)=>{
+    if(index%2===0){
+        listSecond.style.backgroundColor="green";
     }
-    else if(i===2){
-        li[i].style.backgroundColor="green";
-    }
-    else{
-        li[i].style.backgroundColor="grey";
-    }
-    li[i].style.fontWeight="bold";
-}
+});
