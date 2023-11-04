9ea89ddd48f0cad6b4399cb48193b68a0746f9e2
Aman <aman01428@gmail.com>
2023-11-05 04:10:08 +0530

    05Nov

--- Delete_Edit.js
+++ Delete_Edit.js
@@ -1,25 +1,38 @@
 let form =document.getElementById('addForm');
 let itemList =document.getElementById('items');
+let filter=document.getElementById("filter");
 
 form.addEventListener("submit",addItem);
 
 itemList.addEventListener('click',removeItem);
 
+itemList.addEventListener("click",editItem);
+
+filter.addEventListener("keyup",filterItems); 
+
 function addItem(e){
     e.preventDefault();
 
     let newItem=document.getElementById("item").value;
+    let newItem1=document.getElementById("item1").value;
+    let a=newItem+" "+newItem1;
 
     let li=document.createElement('li');
     li.className="list-group-item";
 
-    li.appendChild(document.createTextNode(newItem));
+    li.appendChild(document.createTextNode(a));
 
     let deleteBtn=document.createElement('button');
     deleteBtn.className='btn btn-danger btn-sm float-right delete';
 
     deleteBtn.appendChild(document.createTextNode('X'));
     li.appendChild(deleteBtn);
+
+    let edit=document.createElement("button");
+    edit.className="btn btn-primary mx-1 btn-sm float-right";
+
+    edit.appendChild(document.createTextNode("Edit"));
+    li.appendChild(edit);
 
     itemList.appendChild(li);
 
@@ -33,4 +46,26 @@ function removeItem(e){
             itemList.removeChild(li);
         }
     }
+}
+
+// function editItem(e){
+//     if(e.target.classList.contains("edit")){
+
+//     }
+// }
+
+function filterItems(e){
+    let text=e.target.value.toLowerCase();
+
+    let items=itemList.getElementsByTagName("li");
+
+    Array.from(items).forEach(function(item){
+        let itemName=item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text)!=-1){
+            item.style.display="block";
+        }
+        else{
+            item.style.display="none";
+        }
+    });
 }
